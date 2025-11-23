a3c01c82c770a6ce260ab45228aac91fb7abe6a3
nryberg <nryberg@gmail.com>
2025-11-23 07:04:53 -0600

    Timezone

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,6 @@ Bundler.require(*Rails.groups)
 module TodoToday
   class Application < Rails::Application
     config.load_defaults 7.0
-    config.time_zone = 'UTC'
+    config.time_zone = 'Central Time (US & Canada)'
   end
 end
